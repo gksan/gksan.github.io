9dab3d17693bc0e86fff3ca56b108044dbd82f9a
gksan <kuturu.sankirth@gmail.com>
2018-02-10 17:07:51 -0500

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+var xLocation= 100;
+var yLocation= 100;
+
 function setup() {
   createCanvas(600, 600);
 }
 
+function keyPressed() {
+  if(keyCode == UP_ARROW) {
+    xLocation = xLocation + 100;
+  }
+}
 function draw() {
-    ellipse(100, 100, 50, 50);
+  //green background
+  background(0 ,255 ,0);
+  // red fill
+  fill(255 ,0 ,0);
+  //edge
+  stroke(0 ,0, 255)
+  //increase edge size
+  strokeWeight(4);
+  //draw ellispse
+    ellipse(xLocation, yLocation, 50, 50);
   }
